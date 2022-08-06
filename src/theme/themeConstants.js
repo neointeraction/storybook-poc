16de9ea2b26a486610428a4eee99fbac75997ff9
Eldhos Aji <eldhosaji1998@gmail.com>
2022-08-06 13:18:40 +0530

    constants variable updates

--- src/theme/themeConstants.js
+++ src/theme/themeConstants.js
@@ -1,15 +1,53 @@
 
 export const fontFamily = {
-    base: "Nunito Sans, Helvetica Neue, Helvetica, Arial, sans-serif",
-    secondary: 'Scope One, Helvetica, Arial',
-  };
+    base: "Public Sans",
+};
 
 const colors = {
-    black:'#000'
+    primary:"#FBB03B",
+    darkGreen:"#30311E",
+    lightGreen:"#936D0A",
+    black:"#1B1B1B",
+    white:"#F5F5F5",
+    grey:rgba(100, 116, 139, 0.6),
+
 }
 
-// color palette
+const spacing = {
+    sp1:'8px',
+    sp2:'16px',
+    sp3:'20px',
+    sp4:'24px',
+    sp5:'32px',
+}
+
+const borderRadius = {
+    r1:'4px',
+    r2:'8px',
+}
+
+const fontWeight = {
+    bold:700,
+    regular:400,
+    semiBold:600,
+    medium:500
+}
+
+const fontSize = {
+     t1:"12px",
+     t2:"14px",
+     t3:"16px",
+     t4:"20px",
+     t5:"28px",
+     t6:"32px",
+}
+
+
 export const theme = {
     colors,
-    fontFamily
+    fontFamily,
+    fontWeight,
+    fontSize,
+    spacing,
+    borderRadius
 }
